0678037c0f67681176f21233eba02cc4961cfe3f
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2023-08-01 16:53:22 -0400

    A2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 import TrackForm from './Components/trackForm.jsx'
 import GetAndEstimateBotton from './Components/getAnEstimateButton.jsx'
 import PrintAndSend from './Components/printAndSendButton.jsx'
-
+import SendoLogoWhite from './imgs/Sendo Logo white.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,10 +12,15 @@ function App() {
   return (
     <>
     <div className='pageOne'>
+
     {/* Navigation ba*/}
       <NavBar />
+    <div className='logoContainer'>
+      <img id='logo' src={SendoLogoWhite} alt="Sendo Logo" />
+    </div>
 <div className='firstTextContainer'>
-      <h1 className='h1FirstText'>Print and Send Documents, Anywhere!</h1>
+      <h1 className='h1FirstText'>Print and Send Documents,</h1>
+      <h1 className='h1FirstText'>Anywhere!</h1>
       <p className='pFirstText'><u>Only in Canada</u></p>
 </div>
 
@@ -26,6 +31,8 @@ function App() {
 <h2 className='h2FirstText'>Track your Order</h2>
 <TrackForm />
 </div>
+
+{/* Buttons*/}
 <div className='buttonsContainer'>
 
 <GetAndEstimateBotton />
--- src/Components/trackForm.jsx
+++ src/Components/trackForm.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import RightArrow from '../imgs/Right Arrow.png'
 import '../Styles/trackForm.css'
+
 function TrackForm () {
+
+const [inputValue, setInputValue] = useState('');
+
+const HandleInputChange = (event) => {
+  
+  const inputValueRegex = /^[0-9]*$/;
+  const { value } = event.target;
+ 
+  if(inputValueRegex.test(value)) {
+  setInputValue(value); 
+  
+}
+};
+
+const handleSubmit = (event) => {
+
+event.preventDefault();
+
+}
+
+const handleClick = () => {
+
+
+}
+
+
+
 return(
 
-<form id="firstBottoms">
+<form id="firstBottoms" onSubmit={handleSubmit}>
         <label for="nombre"></label>
-        <input  type="text" id="trackBar" name="nombre" placeholder="Enter your order number 000-00-000-00" required />
+        <input pattern="[0-9]*" value={inputValue} onChange={HandleInputChange} type="text" id="trackBar" name="nombre" placeholder="Enter your order number 0000000000" required />
       
-      <button type="submit" id="trackButton"><img id="rightArrow" alt="Right arrow logo" src={RightArrow} /></button>
+      <button onClick={handleClick} type="submit" id="trackButton"><img id="rightArrow" alt="Right arrow logo" src={RightArrow} /></button>
       </form>
 )
 };
