1788307fa7d1717bec75559267399c614f1d7f4f
Jose Arias <115059715+JoseArias31@users.noreply.github.com>
2023-08-09 17:43:56 -0400

    A2

--- src/Components/printAndSendForm.jsx
+++ src/Components/printAndSendForm.jsx
@@ -2,7 +2,9 @@
 import NavBar from "./NavBar";
 import '../Styles/PrintAndSendForm.css'
 import SendoLogoBlack from "../imgs/Sendo Logo black.png";
-
+import Igb from '../imgs/Instagram black.png'
+import Wab from '../imgs/whatsapp icon black.png'
+import Emb from '../imgs/Instagram black.png'
 function PrintAndSendForm() {
 
 return(
@@ -12,39 +14,68 @@ return(
     <div className="logoContainer">
         <img id="logo" src={SendoLogoBlack} alt="Sendo Logo" />
       </div>
+      <div className="titleFormContainer">
+      <h1>Print and Send Form</h1>
+      </div>
 <form action="#" method="post" id="formSection">
-      <label htmlFor="shipper-name">Shippers Full Name:</label>
+      
+      <div className="FormRows">
+        <div className="inputLabelContainer">
+      <label className="formLabels" htmlFor="shipper-name">Shippers Full Name:</label>
       <input className="formSpace" type="text1" id="shipper-name" name="shipper-name" required />
-      
-      <label htmlFor="shipper-email">Email Address:</label>
+      </div>
+      <div className="inputLabelContainer">
+      <label className="formLabels" htmlFor="shipper-email">Email Address:</label>
       <input className="formSpace" type="email" id="shipper-email" name="shipper-email" required />
-
-      <label htmlFor="recipient-phone">Phone Number:</label>
+      </div>
+      </div>
+      <div className="FormRows">
+      <div className="inputLabelContainer">
+      <label className="formLabels" htmlFor="recipient-phone">Phone Number:</label>
       <input className="formSpace" type="tel" id="recipient-phone" name="recipient-phone" required />
-      
-      <label htmlFor="files">Add Files:</label>
+      </div>
+      <div className="inputLabelContainer">
+      <label className="formLabels" htmlFor="files">Add Files:</label>
       <input className="formSpace" type="file" id="files" name="files" />
-      
-      <label htmlFor="recipient-name">Recipients Full Name:</label>
+      </div>
+      </div>
+      <div className="FormRows">
+        <div className="inputLabelContainer">
+      <label className="formLabels" htmlFor="recipient-name">Recipients Full Name:</label>
       <input className="formSpace" type="text1" id="recipient-name" name="recipient-name" required />
-      
-      <label htmlFor="recipient-address">Full Address:</label>
+      </div>
+      <div className="inputLabelContainer">
+      <label className="formLabels" htmlFor="recipient-address">Full Address:</label>
       <input className="formSpace" type="text1" id="recipient-address" name="recipient-address" required />
-      
-      <label htmlFor="recipient-email">Email Address:</label>
+      </div>
+      </div>
+      <div className="FormRows">
+        <div className="inputLabelContainer">
+      <label className="formLabels" htmlFor="recipient-email">Email Address:</label>
       <input className="formSpace" type="email" id="recipient-email" name="recipient-email" required />
-      
-      <label htmlFor="recipient-phone">Phone Number:</label>
+      </div>
+      <div className="inputLabelContainer">
+      <label className="formLabels" htmlFor="recipient-phone">Phone Number:</label>
       <input className="formSpace" type="tel" id="recipient-phone" name="recipient-phone" required />
-      
+      </div>
+      </div>
       <input className="formSubmit" type="submit" value="Submit" />
     </form>
 
-<div>
-    <a href="https://instagram.com/gojosearias?igshid=YmMyMTA2M2Y=" rel="noopener"><img src="Instagram White Logo.png" alt="Instagram" className="social" width="66" /></a>
-    <a href="https://api.whatsapp.com/send?phone=16474252986&text=Welcome to SenDo, talk to us for a Quote!" rel="noopener"><img src="WhatsApp White logo.png" alt="WhatsApp" className="social" width="66" /></a>
-    <a href="mailto:gojosearias@gmail.com" rel="noopener"><img src="Email white logo.png" alt="Email" className="social" width="66" /></a>
+<div className="footerPrintAndSendForm">
+ 
+ <div className="socialIcons">
+    <a href="https://instagram.com/gojosearias?igshid=YmMyMTA2M2Y=" rel="noopener"><img src={Igb} alt="Instagram" className="social" width="66" /></a>
+    <a href="https://api.whatsapp.com/send?phone=16474252986&text=Welcome to SenDo, talk to us for a Quote!" rel="noopener"><img src={Wab} alt="WhatsApp" className="social" width="66" /></a>
+    <a href="mailto:gojosearias@gmail.com" rel="noopener"><img src={Emb} alt="Email" className="social" width="66" /></a>
+ </div>
+<div className="copyRight">
+<p id="copyRight">SenDo© - 2023 - All Rights Reserved</p>
 </div>
+<div className="phoneNumber">
+    <p id="phoneNumber">647-425-2986</p>
+</div>
+    </div>
 
 </div>
     
